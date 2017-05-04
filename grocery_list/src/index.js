16cdf12c6f9d5557624569325133f515ffa97987
Jerry Gao <gao@blackwright.com>
2017-05-04 12:32:29 -0400

    finish grocery warmup

--- grocery_list/src/index.js
+++ grocery_list/src/index.js
@@ -5,3 +5,48 @@ import './index.css';
 
 // Import the createStore method from Redux
 import { createStore } from 'redux';
+
+import { groceryApp } from './reducers';
+
+import {
+  addItem,
+  setPurchasedFilter,
+  setCategoryFilter,
+  setSort
+} from './actions';
+
+let store = createStore(groceryApp);
+
+let unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
+
+console.log('initial store state:', store.getState());
+
+store.dispatch(addItem({
+  name: 'Tasty cucumber',
+  description: 'It is tasty',
+  category: 'Weapons',
+  purchased: true,
+  amount: 13
+}));
+
+store.dispatch(addItem({
+  name: 'Healthy cucumber',
+  description: 'It is healthy',
+  category: 'Weapons',
+  purchased: false,
+  amount: 20
+}));
+
+store.dispatch(addItem({
+  name: 'Gross cucumber',
+  description: 'It is gross',
+  category: 'Toys',
+  purchased: true,
+  amount: 6
+}));
+
+store.dispatch(setPurchasedFilter('PURCHASED'));
+
+unsubscribe();
